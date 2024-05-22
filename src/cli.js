0fb274e9ecc9ba6de63e652908748657f0fefdba
Oxana Sorokina <oxana.copokina@yandex.ru>
2024-05-23 00:13:49 +0300

    install the readline-sync library

---
+++ src/cli.js
@@ -0,0 +1,13 @@
+import readlineSync from 'readline-sync';
+
+
+
+
+console.log("Welcome to the Brain Games!");
+
+
+// Wait for user's response.
+const userName = readlineSync.question('May I have your name? ');
+console.log('Hello, ' + userName + '!');
+
+export default userName;
